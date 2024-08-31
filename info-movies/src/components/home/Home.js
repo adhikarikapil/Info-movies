aca41d4482c4e8e7c720f7f1f7940eb5cf6a0036
Kapil Adhikari <kapiladhikari2060@gmail.com>
2024-08-31 15:40:47 +0545

    Api handled

--- info-movies/src/components/home/Home.js
+++ info-movies/src/components/home/Home.js
@@ -1,4 +1,6 @@
 import React from "react";
+// API
+import API from "../../API";
 
 // Config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../../config";
@@ -10,11 +12,29 @@ import { useState, useEffect } from "react";
 
 // Image
 import NoImage from "../../images/no_image.jpg";
+import apiSettings from "../../API";
 
 const Home = () => {
   const [state, setState] = useState();
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
+
+  const fetchMovies = async (page, searchTearm = "") => {
+    try {
+      setError(false);
+      setLoading(true);
+
+      const movies = await API.fetchMovies(searchTearm, page);
+      console.log(movies);
+    } catch (error) {
+      setError(true);
+    }
+  };
+
+  // Initial Render
+  useEffect(() => {
+    fetchMovies(1);
+  }, []);   // [] is called dependency array
 
   return (
     <>
